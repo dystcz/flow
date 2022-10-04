ef865f903e45e1eb0c36035a123c250c38b8a4a2
Jakub Theimer <jakub@dy.st>
2022-10-04 02:51:36 +0200

    remove unused sp method calls

--- src/ProcessServiceProvider.php
+++ src/ProcessServiceProvider.php
@@ -9,35 +9,19 @@ class ProcessServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(): void
     {
         /*
          * Optional methods to load your package assets
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'process');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'process');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('process.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/process'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/process'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/process'),
-            ], 'lang');*/
 
             // Registering package commands.
             // $this->commands([]);
@@ -47,14 +31,14 @@ class ProcessServiceProvider extends ServiceProvider
     /**
      * Register the application services.
      */
-    public function register()
+    public function register(): void
     {
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'process');
 
         // Register the main class to use with the facade
         $this->app->singleton('process', function () {
-            return new Process;
+            return new Process();
         });
     }
 }
