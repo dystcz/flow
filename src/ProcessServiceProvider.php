e8cf920de647afcb13513a4d3506b968c0e53dcc
Jakub Theimer <jakub@dy.st>
2022-11-13 22:31:14 +0100

    publish migrations

--- src/ProcessServiceProvider.php
+++ src/ProcessServiceProvider.php
@@ -15,12 +15,12 @@ class ProcessServiceProvider extends ServiceProvider
          * Optional methods to load your package assets
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'process');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__ . '/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('process.php'),
+                __DIR__ . '/../config/config.php' => config_path('process.php'),
             ], 'config');
 
             // Registering package commands.
@@ -34,7 +34,7 @@ class ProcessServiceProvider extends ServiceProvider
     public function register(): void
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'process');
+        $this->mergeConfigFrom(__DIR__ . '/../config/process.php', 'process');
 
         // Register the main class to use with the facade
         $this->app->singleton('process', function () {
