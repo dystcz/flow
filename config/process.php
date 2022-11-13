2425fc8929c925a3542cdf27492ae702982ed16e
Jakub Theimer <jakub@dy.st>
2022-11-14 00:33:50 +0100

    add configurable collections, observers, notifications

--- config/process.php
+++ config/process.php
@@ -3,19 +3,31 @@
 return [
 
     'processes' => [
+
         // Process model class
         'model' => Dystcz\Process\Models\Process::class,
+        'observer' => Dystcz\Process\Observers\ProcessObserver::class,
+        'collection' => Dystcz\Process\Collections\ProcessCollection::class,
     ],
 
     'process_config' => [
+
         // Process config model class
         'model' => Dystcz\Process\Models\ProcessConfig::class,
+        'collection' => Dystcz\Process\Collections\ProcessConfigCollection::class,
 
         // Model class for responsible people
         'responsible_person_model' => Domain\Users\Models\User::class,
 
         // Model class for notifiable people
         'notifiable_person_model' => Domain\Users\Models\User::class,
+    ],
+
+    'notifications' => [
+
+        // Default notification channels
+        'default_channels' => ['mail', 'database'],
+
     ],
 
 ];
