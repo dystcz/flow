f89f5b1fc0a8fb20f285cec484f228baf4f8a510
Jakub Theimer <jakub@dy.st>
2022-11-14 00:47:38 +0100

    add abstract process request

---
+++ src/Http/Requests/ProcessRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dystcz\Process\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class ProcessRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
