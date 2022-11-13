f0b20a04081f95d1d53cd08b73a56099b8702f2f
Jakub Theimer <jakub@dy.st>
2022-11-14 00:33:13 +0100

    process config wip

--- src/Models/ProcessConfig.php
+++ src/Models/ProcessConfig.php
@@ -2,13 +2,38 @@
 
 namespace Dystcz\Process\Models;
 
+use Dystcz\Process\Collections\ProcessConfigCollection;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Config;
 use Kalnoy\Nestedset\NodeTrait;
 
 class ProcessConfig extends Model
 {
     use NodeTrait;
+    use SoftDeletes;
+
+    /**
+     * Register Process config eloquent collection.
+     *
+     * @param array $models
+     * @return ProcessCollection
+     */
+    public function newCollection(array $models = []): ProcessConfigCollection
+    {
+        return new ProcessConfigCollection($models);
+    }
+
+    /**
+     * Processes that need to be finished in order to open this process.
+     *
+     * @return HasMany
+     */
+    public function blockingProcesses(): HasMany
+    {
+        return $this->hasMany(Config::get('process.process_config.model'));
+    }
 
     /**
      * Responsible people relation.
