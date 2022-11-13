bdd1efd25b3bd57b03070ce10f4bee9a2de0938a
Jakub Theimer <jakub@dy.st>
2022-11-13 22:31:02 +0100

    add nested set trait

--- src/Models/Process.php
+++ src/Models/Process.php
@@ -3,9 +3,11 @@
 namespace Dystcz\Process\Models;
 
 use Dystcz\Process\Traits\InteractsWithHandler;
+use Kalnoy\Nestedset\NodeTrait;
 use ProcessContract;
 
 class Process extends Model implements ProcessContract
 {
     use InteractsWithHandler;
+    use NodeTrait;
 }
