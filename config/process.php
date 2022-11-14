482367e3193db270d3261acea7744b22fa9247a4
Jakub Theimer <jakub@dy.st>
2022-11-14 01:04:50 +0100

    add templates, rename config group

--- config/process.php
+++ config/process.php
@@ -6,21 +6,42 @@ return [
 
         // Process model class
         'model' => Dystcz\Process\Models\Process::class,
-        'observer' => Dystcz\Process\Observers\ProcessObserver::class,
+
+        // Process collection class
         'collection' => Dystcz\Process\Collections\ProcessCollection::class,
+
+        // Process observer class
+        'observer' => Dystcz\Process\Observers\ProcessObserver::class,
     ],
 
-    'process_config' => [
+    'config' => [
 
         // Process config model class
         'model' => Dystcz\Process\Models\ProcessConfig::class,
+
+        // Process config collection class
         'collection' => Dystcz\Process\Collections\ProcessConfigCollection::class,
+
+        // Process config observer class
+        'observer' => Dystcz\Process\Observers\ProcessConfigObserver::class,
 
         // Model class for responsible people
         'responsible_person_model' => Domain\Users\Models\User::class,
 
         // Model class for notifiable people
         'notifiable_person_model' => Domain\Users\Models\User::class,
+    ],
+
+    'templates' => [
+
+        // Process template model class
+        'model' => Dystcz\Process\Models\ProcessTemplate::class,
+
+        // Process template collection class
+        'collection' => Dystcz\Process\Collections\ProcessTemplateCollection::class,
+
+        // Process template observer class
+        'observer' => Dystcz\Process\Observers\ProcessObserver::class,
     ],
 
     'notifications' => [
