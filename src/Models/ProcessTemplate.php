5f49b3567886cc926a086a76eb54b2c574779b59
Jakub Theimer <jakub@dy.st>
2022-11-14 01:19:49 +0100

    add process nodes relation

--- src/Models/ProcessTemplate.php
+++ src/Models/ProcessTemplate.php
@@ -3,7 +3,9 @@
 namespace Dystcz\Process\Models;
 
 use Dystcz\Process\Collections\ProcessTemplateCollection;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Config;
 
 class ProcessTemplate extends Model
 {
@@ -18,5 +20,15 @@ class ProcessTemplate extends Model
     public function newCollection(array $models = []): ProcessTemplateCollection
     {
         return new ProcessTemplateCollection($models);
+    }
+
+    /**
+     * ProcessOptions relationship.
+     *
+     * @return HasMany
+     */
+    public function processNodes(): HasMany
+    {
+        return $this->hasMany(Config::get('process.nodes.model'));
     }
 }
