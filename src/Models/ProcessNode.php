c6f952b2fe33b7e0fa342c5362a4903cf956e7c3
Jakub Theimer <jakub@dy.st>
2022-11-14 01:19:42 +0100

    rename process config to process node

--- src/Models/ProcessNode.php
+++ src/Models/ProcessNode.php
@@ -2,37 +2,48 @@
 
 namespace Dystcz\Process\Models;
 
-use Dystcz\Process\Collections\ProcessConfigCollection;
+use Dystcz\Process\Collections\ProcessNodeCollection;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Config;
 use Kalnoy\Nestedset\NodeTrait;
 
-class ProcessConfig extends Model
+class ProcessNode extends Model
 {
     use NodeTrait;
     use SoftDeletes;
 
     /**
-     * Register Process config eloquent collection.
+     * Register ProcessNodeeloquent collection.
      *
      * @param array $models
-     * @return ProcessCollection
+     * @return ProcessNodeCollection
      */
-    public function newCollection(array $models = []): ProcessConfigCollection
+    public function newCollection(array $models = []): ProcessNodeCollection
     {
-        return new ProcessConfigCollection($models);
+        return new ProcessNodeCollection($models);
     }
 
     /**
-     * Processes that need to be finished in order to open this process.
+     * ProcessTemplate relationship.
+     *
+     * @return BelongsTo
+     */
+    public function template(): BelongsTo
+    {
+        return $this->belongsTo(Config::get('process.templates.model'));
+    }
+
+    /**
+     * Nodes that need to be finished in order to open this process.
      *
      * @return HasMany
      */
-    public function blockingProcesses(): HasMany
+    public function blockingNodes(): HasMany
     {
-        return $this->hasMany(Config::get('process.config.model'));
+        return $this->hasMany(Config::get('process.nodes.model'));
     }
 
     /**
