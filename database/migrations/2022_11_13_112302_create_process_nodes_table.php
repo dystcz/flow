6cd1d9c4d24c325233e6f4a2558d18685651f4ad
Jakub Theimer <jakub@dy.st>
2023-01-29 03:58:30 +0800

    add process nodes table

---
+++ database/migrations/2022_11_13_112302_create_process_nodes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('process_nodes', function (Blueprint $table) {
+            $table->id();
+
+            // ProcessTemplate relationship
+            $table->unsignedBigInteger('process_template_id');
+            $table->foreign('process_template_id')->references('id')->on('process_templates');
+
+            // Handler class
+            $table->string('handler');
+
+            $table->string('title');
+            $table->string('key');
+            $table->string('group');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('process_nodes');
+    }
+};
