423ec51069fe9765a554e273cd17f418350a01fa
Jakub Theimer <jakub@dy.st>
2023-01-29 03:58:47 +0800

    add processes table

---
+++ database/migrations/2022_11_13_212302_create_processes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('processes', function (Blueprint $table) {
+            $table->id();
+            $table->nestedSet();
+
+            // Handler class
+            $table->string('handler');
+
+            $table->string('title');
+            $table->string('key');
+            $table->string('group');
+
+            $table->boolean('open')->default(true);
+            $table->boolean('finished')->default(false);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('processes');
+    }
+};
