f95375adbeb3e753c85652c1afaf79e8d256e532
Jakub Theimer <jakub@dy.st>
2023-02-01 03:43:16 +0800

    add process handler contract

---
+++ src/Contracts/ProcessHandlerContract.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dystcz\Process\Contracts;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+interface ProcessHandlerContract
+{
+    // /**
+    //  * Handle process save.
+    //  *
+    //  * @param ProcessRequestContract $request
+    //  * @return Response|JsonResponse
+    //  */
+    // public function handle(ProcessRequestContract $request): Response|JsonResponse;
+}
