14e612f287426fe6e5da3e2724375943be70d6ef
Jakub Theimer <jakub@dy.st>
2023-02-02 04:43:41 +0800

    add docblocks

--- src/Http/Controllers/ProcessTemplatesController.php
+++ src/Http/Controllers/ProcessTemplatesController.php
@@ -2,13 +2,22 @@
 
 namespace Dystcz\Process\Http\Controllers;
 
+use Dystcz\Process\Http\Resources\ProcessTemplateResource;
 use Dystcz\Process\Models\ProcessTemplate;
+use Illuminate\Http\JsonResponse;
 
 class ProcessTemplatesController extends Controller
 {
-    public function index()
+    /**
+     * List process templates.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
     {
         $templates = ProcessTemplate::query()
             ->get();
+
+        return ProcessTemplateResource::collection($templates);
     }
 }
