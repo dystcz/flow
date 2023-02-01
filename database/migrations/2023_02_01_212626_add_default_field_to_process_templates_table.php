7178eb4927dd1c230de394e7765ce26f97e26d84
Jakub Theimer <jakub@dy.st>
2023-02-02 05:28:10 +0800

    add default field to process templates table

---
+++ database/migrations/2023_02_01_212626_add_default_field_to_process_templates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('process_templates', function (Blueprint $table) {
+            $table->boolean('default')->after('title')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('process_templates', function (Blueprint $table) {
+            $table->dropColumn('default');
+        });
+    }
+};
