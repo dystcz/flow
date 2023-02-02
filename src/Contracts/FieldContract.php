c3d4a5a50593b206c756d72f2d6f1ca082fd8b5b
Jakub Theimer <jakub@dy.st>
2023-02-03 03:41:24 +0800

    add methods

--- src/Contracts/FieldContract.php
+++ src/Contracts/FieldContract.php
@@ -4,4 +4,17 @@ namespace Dystcz\Process\Contracts;
 
 interface FieldContract
 {
+    /**
+     * Set value.
+     *
+     * @return self
+     */
+    public function setValue(mixed $value): self;
+
+    /**
+     * Get value.
+     *
+     * @return mixed
+     */
+    public function getValue(): mixed;
 }
