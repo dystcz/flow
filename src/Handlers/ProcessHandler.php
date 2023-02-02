5fd56bf8967c392277f2dda89f8c6cc78dd28283
Jakub Theimer <jakub@dy.st>
2023-02-02 23:20:22 +0800

    refactor abstract handler

--- src/Handlers/ProcessHandler.php
+++ src/Handlers/ProcessHandler.php
@@ -2,27 +2,32 @@
 
 namespace Dystcz\Process\Handlers;
 
-use Dystcz\Process\Collections\ProcessCollection;
+use Dystcz\Process\Contracts\Processable;
 use Dystcz\Process\Contracts\ProcessHandlerContract;
 use Dystcz\Process\Models\Process;
-use Dystcz\Process\Models\ProcessNode;
-use Dystcz\Process\Models\ProcessTemplate;
-use Dystcz\Process\Notifications\ProcessNotification;
-use Illuminate\Support\Collection;
-use Lorisleiva\Actions\Concerns\AsAction;
+use Dystcz\Process\Traits\HandlesAuthorization;
+use Dystcz\Process\Traits\HandlesValidation;
 
 abstract class ProcessHandler implements ProcessHandlerContract
 {
-    use AsAction;
+    use HandlesAuthorization;
+    use HandlesValidation;
 
-    protected ProcessNotification $notification;
-
-    protected ProcessNode $node;
-
-    protected ProcessTemplate $template;
+    public function __construct(public Process $process)
+    {
+    }
 
     /**
-     * Define process fields.
+     * Handle the process.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+    }
+
+    /**
+     * Define process field.
      *
      * @return array
      */
@@ -32,143 +37,22 @@ abstract class ProcessHandler implements ProcessHandlerContract
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * Get process.
      *
-     * @return array
+     * @return Process
      */
-    public function rules(): array
+    public function getProcess(): Process
     {
-        return Collection::make($this->fields())->mapWithKeys(
-            fn ($field) => [$field->key => $field->rules]
-        )->toArray();
+        return $this->process;
     }
 
     /**
-     * Get custom messages for validator errors.
+     * Get model.
      *
-     * @return array
+     * @return Process
      */
-    public function messages(): array
+    public function getModel(): Processable
     {
-        return Collection::make($this->fields())->mapWithKeys(
-            fn ($field) => Collection::make($field->messages)->mapWithKeys(
-                fn ($message, $rule) => ["{$field->key}.{$rule}" => $message]
-            )->toArray()
-        )->toArray();
-    }
-
-    /**
-     * Check that all blocking processes are finished.
-     *
-     * @return bool
-     */
-    public function initialisable(): bool
-    {
-        // If there are no unfinished blocking processes, return true.
-        return $this->getUnfinishedBlockingProcesses()->isEmpty();
-    }
-
-    /**
-     * Get all blocking processes.
-     *
-     * @return ProcessCollection
-     */
-    public function getBlockingProcesses(): ProcessCollection
-    {
-        $blocking = $this->process->node->blockingNodes;
-
-        // Get all processes in the tree
-        // Filter out those, that are blocking
-
-        return $blocking;
-    }
-
-    /**
-     * Get all unfinished blocking processes.
-     *
-     * @return ProcessCollection
-     */
-    public function getUnfinishedBlockingProcesses(): ProcessCollection
-    {
-        return $this->getBlockingProcesses()->filter(
-            fn ($process) => !$process->isFinished()
-        );
-    }
-
-    /**
-     * Get collection of prev processes.
-     *
-     * @return ProcessCollection<Process>
-     */
-    public function prevProcesses(): ProcessCollection
-    {
-        // TODO: Implement retrieval of previous processes from process node tree.
-        return new ProcessCollection();
-    }
-
-    /**
-     * Get collection of next processes.
-     *
-     * @return ProcessCollection<Process>
-     */
-    public function nextProcesses(): ProcessCollection
-    {
-        // TODO: Implement retrieval of next processes from process node tree.
-        return new ProcessCollection();
-    }
-
-    /**
-     * Handle the Process "initialized" event.
-     *
-     * @param  Process  $process
-     * @return void
-     */
-    protected function initialized(Process $process): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Process "updated" event.
-     *
-     * @param  Process  $process
-     * @return void
-     */
-    protected function updated(Process $process): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Process "closed" event.
-     *
-     * @param  Process  $process
-     * @return void
-     */
-    protected function closed(Process $process): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Process "reopened" event.
-     *
-     * @param  Process  $process
-     * @return void
-     */
-    protected function reopened(Process $process): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Process "finished" event.
-     *
-     * @param  Process  $process
-     * @return void
-     */
-    protected function finished(Process $process): void
-    {
-        //
+        return $this->process->model;
     }
 }
