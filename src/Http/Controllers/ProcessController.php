02f36f07405b59edaf383214b29a0d112a976cf7
Jakub Theimer <jakub@dy.st>
2023-02-03 02:20:04 +0800

    add show controller, refactor controllers

--- src/Http/Controllers/ProcessController.php
+++ src/Http/Controllers/ProcessController.php
@@ -4,6 +4,7 @@ namespace Dystcz\Process\Http\Controllers;
 
 use Dystcz\Process\Handlers\ProcessHandler;
 use Dystcz\Process\Http\Requests\ProcessRequest;
+use Dystcz\Process\Models\Process;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\ValidationException;
@@ -18,15 +19,15 @@ class ProcessController extends Controller
      * @throws ValidationException
      * @throws AuthorizationException
      */
-    public function __invoke(ProcessRequest $request): JsonResponse
+    public function __invoke(ProcessRequest $request, Process $process): JsonResponse
     {
         /** @var ProcessHandler $handler */
-        $handler = $request->getHandler();
+        $handler = $process->handler();
 
-        $handler::validateProcess($request);
         $handler::authorizeToHandleProcess($request);
+        $handler::validateProcess($request);
 
-        $handler->handle();
+        $handler->handle($request);
 
         // Dispatch process handled event
 
--- src/Http/Controllers/ProcessShowController.php
+++ src/Http/Controllers/ProcessShowController.php
@@ -4,24 +4,21 @@ namespace Dystcz\Process\Http\Controllers;
 
 use Dystcz\Process\Handlers\ProcessHandler;
 use Dystcz\Process\Http\Requests\ProcessRequest;
-use Dystcz\Process\Http\Resources\FieldResource;
 use Dystcz\Process\Http\Resources\ProcessResource;
 use Dystcz\Process\Models\Process;
 use Illuminate\Http\JsonResponse;
 
-class ProcessEditController extends Controller
+class ProcessShowController extends Controller
 {
     public function __invoke(ProcessRequest $request, Process $process): JsonResponse
     {
         /** @var ProcessHandler $handler */
         $handler = $process->handler();
 
-        $handler::authorizeToEditProcess($request);
-        $fields = $handler::setFieldValuesFromProcess($request);
+        $handler->authorizeToViewProcess($request);
 
         return new JsonResponse([
             'process' => new ProcessResource($process),
-            'fields' => FieldResource::collection($fields),
         ], 200);
     }
 }
