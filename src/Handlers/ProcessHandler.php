ed2a1fedf0a305022d348dab78a79f5305cdd0d6
Jakub Theimer <jakub@dy.st>
2023-02-03 04:41:16 +0800

    extract logic to traits

--- src/Handlers/ProcessHandler.php
+++ src/Handlers/ProcessHandler.php
@@ -2,19 +2,22 @@
 
 namespace Dystcz\Process\Handlers;
 
-use Dystcz\Process\Contracts\Processable;
 use Dystcz\Process\Contracts\ProcessHandlerContract;
 use Dystcz\Process\Http\Requests\ProcessRequest;
 use Dystcz\Process\Models\Process;
 use Dystcz\Process\Traits\HandlesAuthorization;
 use Dystcz\Process\Traits\HandlesFields;
 use Dystcz\Process\Traits\HandlesValidation;
+use Dystcz\Process\Traits\InteractsWithModel;
+use Dystcz\Process\Traits\InteractsWithProcess;
 
 abstract class ProcessHandler implements ProcessHandlerContract
 {
     use HandlesAuthorization;
-    use HandlesValidation;
     use HandlesFields;
+    use HandlesValidation;
+    use InteractsWithModel;
+    use InteractsWithProcess;
 
     public function __construct(public Process $process)
     {
@@ -27,9 +30,7 @@ abstract class ProcessHandler implements ProcessHandlerContract
      */
     public function handle(ProcessRequest $request): void
     {
-        $data = $this->setFieldValuesFromRequest($request);
-
-        $this->saveFieldData($data);
+        $this->saveFieldData($this->setFieldValuesFromRequest($request));
     }
 
     /**
@@ -40,25 +41,5 @@ abstract class ProcessHandler implements ProcessHandlerContract
     protected static function newHandler()
     {
         return new static(new Process);
-    }
-
-    /**
-     * Get process.
-     *
-     * @return Process
-     */
-    public function getProcess(): Process
-    {
-        return $this->process;
-    }
-
-    /**
-     * Get model.
-     *
-     * @return Process
-     */
-    public function getModel(): Processable
-    {
-        return $this->process->model;
     }
 }
