2dd662fd19ad2292220cb24a49797f527746fef4
Jakub Theimer <jakub@dy.st>
2023-02-03 03:41:56 +0800

    add interacts with media trait

---
+++ src/Traits/InteractsWithMedia.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dystcz\Process\Traits;
+
+use Dystcz\Process\Contracts\MediaFieldContract;
+use Spatie\MediaLibrary\InteractsWithMedia as MediaLibraryInteractsWithMedia;
+
+trait InteractsWithMedia
+{
+    use MediaLibraryInteractsWithMedia;
+
+    /**
+     * Save media.
+     *
+     * @param MediaFieldContract $field
+     * @return void
+     */
+    public function saveMedia(MediaFieldContract $field): void
+    {
+        $this->addMedia($field->getValue())->toMediaCollection($field->collection);
+    }
+}
