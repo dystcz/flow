0ccda1239654614f2ccb2c48fb0de36ec5c7b7d3
Jakub Theimer <jakub@dy.st>
2023-02-03 04:41:37 +0800

    use this trait for handler instead

--- src/Traits/InteractsWithProcess.php
+++ src/Traits/InteractsWithProcess.php
@@ -2,39 +2,17 @@
 
 namespace Dystcz\Process\Traits;
 
+use Dystcz\Process\Models\Process;
+
 trait InteractsWithProcess
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * Get process.
      *
-     * @return bool
+     * @return Process
      */
-    public function authorize()
+    public function getProcess(): Process
     {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
-
-    /**
-     * Get custom messages for validator errors.
-     *
-     * @return array
-     */
-    public function messages()
-    {
-        return [
-            //
-        ];
+        return $this->process;
     }
 }
