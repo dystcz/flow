3e2cd9a9b21ebe1a9e1c38f71f5761f3290c13da
Jakub Theimer <jakub@dy.st>
2023-02-02 23:20:41 +0800

    refactor process request

--- src/Http/Requests/ProcessRequest.php
+++ src/Http/Requests/ProcessRequest.php
@@ -3,41 +3,10 @@
 namespace Dystcz\Process\Http\Requests;
 
 use Dystcz\Process\Contracts\ProcessRequestContract;
+use Dystcz\Process\Traits\InteractsWithProcess;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ProcessRequest extends FormRequest implements ProcessRequestContract
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
-
-    /**
-     * Get custom messages for validator errors.
-     *
-     * @return array
-     */
-    public function messages()
-    {
-        return [
-            //
-        ];
-    }
+    use InteractsWithProcess;
 }
