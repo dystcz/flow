6b17e5c4ebe845ea523a7b8883cef784359d6662
Jakub Theimer <jakub@dy.st>
2023-02-02 23:23:07 +0800

    add process request interface methods

--- src/Contracts/ProcessRequestContract.php
+++ src/Contracts/ProcessRequestContract.php
@@ -4,4 +4,19 @@ namespace Dystcz\Process\Contracts;
 
 interface ProcessRequestContract
 {
+    /**
+     * Set handler.
+     *
+     * @param ProcessHandlerContract $handler
+     * @return void
+     */
+    public function setHandler(ProcessHandlerContract $handler): void;
+
+    /**
+     * Get handler.
+     *
+     * @param mixed $handler
+     * @return ProcessHandlerContract
+     */
+    public function getHandler(): ProcessHandlerContract;
 }
