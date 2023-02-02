b38f1bb718ec85746de63607aa871f2c15d48054
Jakub Theimer <jakub@dy.st>
2023-02-03 02:19:28 +0800

    remove methods

--- src/Contracts/ProcessRequestContract.php
+++ src/Contracts/ProcessRequestContract.php
@@ -4,19 +4,4 @@ namespace Dystcz\Process\Contracts;
 
 interface ProcessRequestContract
 {
-    /**
-     * Set handler.
-     *
-     * @param ProcessHandlerContract $handler
-     * @return void
-     */
-    public function setHandler(ProcessHandlerContract $handler): void;
-
-    /**
-     * Get handler.
-     *
-     * @param mixed $handler
-     * @return ProcessHandlerContract
-     */
-    public function getHandler(): ProcessHandlerContract;
 }
