782f8a41ab41ac3376f26895a372e09ab166f809
Jakub Theimer <jakub@dy.st>
2023-02-06 23:49:45 +0800

    register finished model event

--- src/Domain/Processes/Traits/HasCustomModelEvents.php
+++ src/Domain/Processes/Traits/HasCustomModelEvents.php
@@ -2,6 +2,8 @@
 
 namespace Dystcz\Process\Domain\Processes\Traits;
 
+use Closure;
+
 trait HasCustomModelEvents
 {
     /**
@@ -9,8 +11,19 @@ trait HasCustomModelEvents
      *
      * @return void
      */
-    public function wasFinished(): void
+    public function fireFinishedEvent(): void
     {
-        $this->fireModelEvent('finished', false);
+        $this->fireModelEvent('finished');
+    }
+
+    /**
+     * Register a finished model event with the dispatcher.
+     *
+     * @param \Illuminate\Events\QueuedClosure|Closure|string|array $callback
+     * @return void
+     */
+    public static function finished($callback)
+    {
+        static::registerModelEvent('finished', $callback);
     }
 }
