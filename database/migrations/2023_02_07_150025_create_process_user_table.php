09f327a405ea8c892ed2e537f901892f3c3db290
Jakub Theimer <jakub@dy.st>
2023-02-07 23:02:58 +0800

    add process user table

---
+++ database/migrations/2023_02_07_150025_create_process_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('process_user', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('process_id')->nullable();
+            $table->foreign('process_id')->references('id')->on('processes');
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('process_user');
+    }
+};
