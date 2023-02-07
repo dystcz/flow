e424897eab4d9d2c3af73888cce9ddb7f6a920b6
Jakub Theimer <jakub@dy.st>
2023-02-08 00:33:11 +0800

    add mark active process method

--- src/Domain/Processes/Collections/ProcessCollection.php
+++ src/Domain/Processes/Collections/ProcessCollection.php
@@ -2,9 +2,28 @@
 
 namespace Dystcz\Process\Domain\Processes\Collections;
 
+use Dystcz\Process\Domain\Processes\Models\Process;
 use Illuminate\Database\Eloquent\Collection;
 
 class ProcessCollection extends Collection
 {
-    //
+    public function markActiveProcess(): self
+    {
+        $this->reverse()->reduce(function ($carry, Process $process) {
+            if ($carry || $process->isFinished()) {
+                $process->setAttribute('active', false);
+
+                return $carry;
+            }
+
+            if (!$carry && $process->isOpen()) {
+                $process->setAttribute('active', true);
+                $carry = true;
+            }
+
+            return $carry;
+        }, false);
+
+        return $this;
+    }
 }
