36a370f807a9254b4deb18f4a146f99039bf4844
Jakub Theimer <jakub@dy.st>
2023-03-16 21:11:38 +0100

    add field group enum

---
+++ src/Domain/Fields/Enums/FieldGroup.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dystcz\Flow\Domain\Fields\Enums;
+
+use Dystcz\Flow\Domain\Base\Traits\ArrayJsonCast;
+use Dystcz\Flow\Domain\Flows\Contracts\EnumContract;
+
+enum FieldGroup: string implements EnumContract
+{
+    use ArrayJsonCast;
+
+    case DATA = 'data';
+    case MISC = 'misc';
+    case EMAIL_TEMPLATES = 'email-templates';
+
+    /**
+     * Get label.
+     */
+    public function label(): string
+    {
+        return __('flow.fields.groups.'.$this->value);
+    }
+
+    /**
+     * Get the instance as an array.
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->value,
+            'title' => $this->label(),
+        ];
+    }
+}
