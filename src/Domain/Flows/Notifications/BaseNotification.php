5b55730b6474c25b55729174182e1b3ec1551212
Jakub Theimer <jakub@dy.st>
2023-03-16 15:01:33 +0100

    abstract notifications

---
+++ src/Domain/Flows/Notifications/BaseNotification.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Dystcz\Flow\Domain\Flows\Notifications;
+
+use Dystcz\Flow\Domain\Flows\Builders\NotificationDataBuilder;
+use Dystcz\Flow\Domain\Flows\Contracts\Notifiable;
+use Dystcz\Flow\Domain\Flows\Contracts\NotificationContract;
+use Dystcz\Flow\Domain\Flows\Data\NotificationData;
+use Illuminate\Bus\Queueable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+
+class BaseNotification extends Notification implements NotificationContract
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public ?Model $model = null,
+        protected ?NotificationData $data = null,
+    ) {
+        $this->afterCommit();
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     */
+    public function via(Notifiable $notifiable): array
+    {
+        if (App::environment(['local', 'staging'])) {
+            return ['database'];
+        }
+
+        return $notifiable->notificationChannels()
+            ?? Config::get('flow.notifications.default_channels', ['database']);
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(Notifiable $notifiable): MailMessage
+    {
+        return new MailMessage;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     */
+    public function toArray(Notifiable $notifiable): array
+    {
+        $data = $this->data ?? $this->getData($notifiable);
+
+        return $data->toArray();
+    }
+
+    /**
+     * Get notification data builder.
+     */
+    public function dataBuilder(): NotificationDataBuilder
+    {
+        return NotificationDataBuilder::from($this->model ?? new Model);
+    }
+
+    /**
+     * Get notification data.
+     */
+    public function getData(): NotificationData
+    {
+        return $this->dataBuilder()->build();
+    }
+}
--- src/Domain/Flows/Notifications/StepNotification.php
+++ src/Domain/Flows/Notifications/StepNotification.php
@@ -4,84 +4,42 @@ declare(strict_types=1);
 
 namespace Dystcz\Flow\Domain\Flows\Notifications;
 
-use Dystcz\Flow\Domain\Flows\Contracts\Notifiable;
+use Dystcz\Flow\Domain\Flows\Builders\NotificationDataBuilder;
+use Dystcz\Flow\Domain\Flows\Builders\StepNotificationDataBuilder;
 use Dystcz\Flow\Domain\Flows\Data\NotificationData;
-use Dystcz\Flow\Domain\Flows\Enums\NotificationType;
 use Dystcz\Flow\Domain\Flows\Models\Step;
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Config;
 
-class StepNotification extends Notification implements ShouldQueue
+class StepNotification extends BaseNotification implements ShouldQueue
 {
-    use Queueable;
-
     /**
      * Create a new notification instance.
      *
      * @return void
      */
     public function __construct(
-        public Step $step,
+        public Step $model,
         protected ?NotificationData $data = null,
     ) {
-        $this->afterCommit();
+        parent::__construct($model, $data);
     }
 
     /**
-     * Get the notification's delivery channels.
+     * Get notification data builder.
      */
-    public function via(Notifiable $notifiable): array
+    public function dataBuilder(): NotificationDataBuilder
     {
-        if (App::environment(['local', 'staging'])) {
-            return ['database'];
-        }
-
-        return $notifiable->notificationChannels() ?? Config::get('flow.notifications.default_channels', ['database']);
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(Notifiable $notifiable): MailMessage
-    {
-        return new MailMessage;
-    }
-
-    /**
-     * Get the array representation of the notification.
-     */
-    public function toArray(Notifiable $notifiable): array
-    {
-        $data = $this->data ?? $this->getData();
-
-        return  $data->toArray();
+        return StepNotificationDataBuilder::from($this->model);
     }
 
     /**
      * Get notification data.
      */
-    protected function getData(): NotificationData
+    public function getData(): NotificationData
     {
-        return new NotificationData(
-            type: NotificationType::NORMAL,
-            subject_id: $this->step->id,
-            subject_type: $this->step::class,
-            description: 'Notification description',
-            body: 'Something happened.',
-            relations: [
-                'step' => [
-                    'id' => $this->step->id,
-                    'name' => $this->step->name,
-                ],
-                'model' => [
-                    'id' => $this->step->model->id,
-                    'name' => $this->step->model->name,
-                ],
-            ],
-        );
+        return $this->dataBuilder()
+            ->setDescription('Notification description')
+            ->setBody('Something happened.')
+            ->build();
     }
 }
