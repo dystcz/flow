b39e5486e0731900b09d623fa2bacca094f1600b
Jakub Theimer <jakub@dy.st>
2023-03-16 21:12:47 +0100

    add base dto

---
+++ src/Domain/Flows/Data/DTO.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dystcz\Flow\Domain\Flows\Data;
+
+use Dystcz\Flow\Domain\Flows\Contracts\DTOContract;
+
+abstract class DTO implements DTOContract
+{
+    /**
+     * Cast to array.
+     */
+    public function toArray(): array
+    {
+        return [];
+    }
+
+    /**
+     * Serialize to json.
+     */
+    public function jsonSerialize(): mixed
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param  int  $options
+     */
+    public function toJson($options = 0): string
+    {
+        return json_encode($this->jsonSerialize(), $options);
+    }
+}
