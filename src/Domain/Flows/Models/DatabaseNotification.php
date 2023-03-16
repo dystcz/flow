9305f82a8f9b5e2361038209438c819a431bf2bf
Jakub Theimer <jakub@dy.st>
2023-03-16 15:36:24 +0100

    add useful scopes

--- src/Domain/Flows/Models/DatabaseNotification.php
+++ src/Domain/Flows/Models/DatabaseNotification.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Dystcz\Flow\Domain\Flows\Models;
 
 use Dystcz\Flow\Domain\Flows\Casts\NotificationDataCast;
+use Dystcz\Flow\Domain\Flows\Contracts\Notifiable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Notifications\DatabaseNotification as BaseDatabaseNotification;
+use Illuminate\Support\Collection;
 
 class DatabaseNotification extends BaseDatabaseNotification
 {
@@ -18,4 +21,30 @@ class DatabaseNotification extends BaseDatabaseNotification
         'data' => NotificationDataCast::class,
         'read_at' => 'datetime',
     ];
+
+    /**
+     * Scope notifications for a model.
+     */
+    public function scopeNotifiable(Builder $builder, Notifiable $model): Builder
+    {
+        return $builder
+            ->where('notifiable_id', $model->id)
+            ->where('notifiable_type', $model::class);
+    }
+
+    /**
+     * Scope notifications for multiple models.
+     *
+     * @param  Collection<Notifiable>  $models
+     */
+    public function scopeNotifiables(Builder $builder, Collection $models): Builder
+    {
+        if ($models->isEmpty()) {
+            return $builder;
+        }
+
+        return $builder
+            ->whereIn('notifiable_id', $models->pluck('id')->toArray())
+            ->where('notifiable_type', $models->first()::class);
+    }
 }
