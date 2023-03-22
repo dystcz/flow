dafc2ac48289cc9b4a07b5372f6be828c1dafd53
Jakub Theimer <jakub@dy.st>
2023-03-22 15:51:36 +0100

    add callback option

--- src/Domain/Flows/Actions/StepAlreadyExistsForNode.php
+++ src/Domain/Flows/Actions/StepAlreadyExistsForNode.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Dystcz\Flow\Domain\Flows\Actions;
 
+use Closure;
 use Dystcz\Flow\Domain\Flows\Models\Node;
 use Dystcz\Flow\Domain\Flows\Models\Step;
 
@@ -11,9 +12,13 @@ class StepAlreadyExistsForNode
 {
     /**
      * Get blocking nodes for a given node.
+     *
+     * @param (Cllosure(Step, Node): bool)|null $condition
      */
-    public function handle(Step $step, Node $node): bool
+    public function handle(Step $step, Node $node, ?Closure $callback = null): bool
     {
-        return $step->model->steps->contains('node_id', $node->id);
+        return $callback
+            ? $callback($step, $node)
+            : $step->model->steps->contains('node_id', $node->id);
     }
 }
