0a283e105222021870ab362a482e32a00e64675d
Jakub Theimer <jakub@dy.st>
2023-03-22 22:20:34 +0100

    format date

--- src/Domain/Fields/Fields/Date.php
+++ src/Domain/Fields/Fields/Date.php
@@ -5,8 +5,25 @@ declare(strict_types=1);
 namespace Dystcz\Flow\Domain\Fields\Fields;
 
 use Dystcz\Flow\Domain\Fields\Contracts\DataFieldContract;
+use Dystcz\Flow\Domain\Flows\Contracts\FlowHandlerContract;
+use Illuminate\Support\Carbon;
 
 class Date extends Field implements DataFieldContract
 {
     public string $component = 'date';
+
+    public function __construct(
+        public string $name,
+        public string $key,
+        public array $options = [],
+        public mixed $value = null,
+        public mixed $formattedValue = null,
+    ) {
+        parent::__construct($name, $key, $options, $value, $formattedValue);
+
+        $this->handleFormat(function (Field $field, FlowHandlerContract $fieldHandler) {
+            // TODO: Make format configurable
+            return $field->getValue() ? Carbon::parse($field->getValue())->format('j. n. Y') : null;
+        });
+    }
 }
