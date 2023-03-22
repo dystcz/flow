bb8dec42b6ff7a5d67188250002ae81627975e8b
Jakub Theimer <jakub@dy.st>
2023-03-22 15:51:43 +0100

    add invokeable handler contract

---
+++ src/Domain/Flows/Contracts/InvokeableHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dystcz\Flow\Domain\Flows\Contracts;
+
+/**
+ * Some flow steps can be invoked on demand, independently of the node graph.
+ */
+interface InvokeableHandler
+{
+}
