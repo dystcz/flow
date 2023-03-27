b81e3910a51f53b071a944d92e1615d86a18113c
Jakub Theimer <jakub@dy.st>
2023-03-27 11:22:50 +0200

    cast value to array on retrieve

--- src/Domain/Fields/Fields/Multiselect.php
+++ src/Domain/Fields/Fields/Multiselect.php
@@ -5,8 +5,29 @@ declare(strict_types=1);
 namespace Dystcz\Flow\Domain\Fields\Fields;
 
 use Dystcz\Flow\Domain\Fields\Contracts\DataFieldContract;
+use Dystcz\Flow\Domain\Flows\Contracts\FlowHandlerContract;
 
 class Multiselect extends Field implements DataFieldContract
 {
     public string $component = 'multiselect';
+
+    public function __construct(
+        public string $name,
+        public string $key,
+        public array $options = [],
+        public mixed $value = null,
+        public mixed $formattedValue = null,
+    ) {
+        parent::__construct($name, $key, $options, $value, $formattedValue);
+
+        $this->handleRetrieve(function (Field $field, FlowHandlerContract $handler) {
+            $value = $this->handler()->retrieve($this, $handler);
+
+            $this->setValue(is_array($value) ? $value : [$value]);
+
+            if ($format = $this->formatCallback) {
+                $this->setFormattedValue($format($this, $handler));
+            }
+        });
+    }
 }
