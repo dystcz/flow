316c1b313ff1841bad4049c2b3b535d6f0cb3062
Jakub Theimer <jakub@dy.st>
2023-03-29 03:17:22 +0200

    make node_id nullable in flow_steps table

---
+++ database/migrations/2023_03_29_011541_make_node_id_nullable_in_flow_steps_table.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table(Config::get('flow.steps.table_name'), function (Blueprint $table) {
+            $table->unsignedBigInteger('node_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table(Config::get('flow.steps.table_name'), function (Blueprint $table) {
+            $table->unsignedBigInteger('node_id')->change();
+        });
+    }
+};
