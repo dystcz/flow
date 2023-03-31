2113a391e54358b9ef8a2189f1ec444011abd4a2
Jakub Theimer <jakub@dy.st>
2023-03-31 23:38:26 +0200

    add methods

--- src/Domain/Flows/Traits/InteractsWithHandler.php
+++ src/Domain/Flows/Traits/InteractsWithHandler.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Dystcz\Flow\Domain\Flows\Traits;
 
+use Dystcz\Flow\Domain\Fields\Contracts\FieldContract;
 use Dystcz\Flow\Domain\Flows\Contracts\FlowHandlerContract;
+use Illuminate\Support\Arr;
 
 trait InteractsWithHandler
 {
@@ -14,5 +16,30 @@ trait InteractsWithHandler
     public function handler(): FlowHandlerContract
     {
         return new $this->handler($this);
+    }
+
+    /**
+     * Get step field by step key and field key.
+     */
+    public function getFields(): array
+    {
+        return $this->handler()->fields();
+    }
+
+    /**
+     * Get step field by field key.
+     */
+    public function getFieldByKey(string $fieldKey): ?FieldContract
+    {
+        /** @var FieldContract|null $field */
+        return Arr::first($this->getFields(), fn (FieldContract $field) => $field->getKey() === $fieldKey);
+    }
+
+    /**
+     * Get step field value by field key.
+     */
+    public function getFieldValue(string $fieldKey): mixed
+    {
+        return $this->getFieldByKey($fieldKey)?->retrieve($this->handler())->getValue();
     }
 }
