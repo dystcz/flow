1c5da86112a010de969bd8bf9edbb56a31a4156e
Jakub Theimer <jakub@dy.st>
2023-04-01 01:53:05 +0200

    mark fiels as loosely required when loose validation strategy

--- src/Domain/Fields/Traits/HasRules.php
+++ src/Domain/Fields/Traits/HasRules.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Dystcz\Flow\Domain\Fields\Traits;
 
 use Dystcz\Flow\Domain\Fields\Fields\Field;
+use Dystcz\Flow\Domain\Flows\Enums\ValidationStrategy;
+use Dystcz\Flow\Domain\Flows\Facades\Flow;
 
 trait HasRules
 {
@@ -17,6 +19,18 @@ trait HasRules
      */
     public function rules(array $rules): Field
     {
+        // Mark fields as loosely required
+        if (
+            // If field is required
+            in_array('required', $rules)
+            // If flow is set to loose validation
+            && Flow::validationStrategy() === ValidationStrategy::LOOSE
+            // If not overriden by strict validation
+            && ! in_array(ValidationStrategy::STRICT->value, $rules)
+        ) {
+            $rules = array_merge($rules, [ValidationStrategy::LOOSE->value]);
+        }
+
         $this->rules = $rules;
 
         return $this;
