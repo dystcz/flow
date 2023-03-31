547b7fa50da59bc6b15e7acc87bfd577626df960
Jakub Theimer <jakub@dy.st>
2023-03-31 23:37:34 +0200

    add methods

--- src/Domain/Flows/Contracts/FlowStepContract.php
+++ src/Domain/Flows/Contracts/FlowStepContract.php
@@ -4,6 +4,27 @@ declare(strict_types=1);
 
 namespace Dystcz\Flow\Domain\Flows\Contracts;
 
+use Dystcz\Flow\Domain\Fields\Contracts\FieldContract;
+
 interface FlowStepContract
 {
+    /**
+     * Initialise the handler class.
+     */
+    public function handler(): FlowHandlerContract;
+
+    /**
+     * Get step field by step key and field key.
+     */
+    public function getFields(): array;
+
+    /**
+     * Get step field by field key.
+     */
+    public function getFieldByKey(string $fieldKey): ?FieldContract;
+
+    /**
+     * Get step field value by field key.
+     */
+    public function getFieldValue(string $fieldKey): mixed;
 }
