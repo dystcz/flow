77f6f64f54148a392ab321f298f2f92540f17265
Jakub Theimer <jakub@dy.st>
2023-04-01 01:53:14 +0200

    rename to strict and loose

--- src/Domain/Flows/Enums/ValidationStrategy.php
+++ src/Domain/Flows/Enums/ValidationStrategy.php
@@ -9,11 +9,11 @@ enum ValidationStrategy: string
     /**
      * All required fields must be filled in order to **save** the step.
      */
-    case STRONG = 'strong';
+    case STRICT = 'strict';
 
     /**
      * All required fields must be filled in order to **complete** the step.
      * Step can be saved even if some required fields are not filled.
      */
-    case WEAK = 'weak';
+    case LOOSE = 'loose';
 }
