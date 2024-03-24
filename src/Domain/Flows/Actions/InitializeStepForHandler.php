4aee01686be7afd9b5a796e39a69221c39b71b88
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-24 22:25:44 +0100

    add initialize step for handler action
    remove ray

---
+++ src/Domain/Flows/Actions/InitializeStepForHandler.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dystcz\Flow\Domain\Flows\Actions;
+
+use Dystcz\Flow\Domain\Flows\Exceptions\NodeDoesNotExistException;
+use Dystcz\Flow\Domain\Flows\Models\Node;
+use Dystcz\Flow\Domain\Flows\Models\Step;
+
+class InitializeStepForHandler
+{
+    protected GetNextNodesForNode $getNextNodes;
+
+    protected StepAlreadyExistsForNode $stepAlreadyExistsForNode;
+
+    protected InitializeStep $initializeStep;
+
+    public function __construct(private Step $step)
+    {
+        $this->getNextNodes = new GetNextNodesForNode();
+
+        $this->stepAlreadyExistsForNode = new StepAlreadyExistsForNode();
+
+        $this->initializeStep = new InitializeStep();
+    }
+
+    /**
+     * Initialize step for a handler.
+     *
+     * @param  class-string  $handler
+     */
+    public function handle(string $handler, bool $onlyChildrenNodes = false): void
+    {
+        $this->step->loadMissing([
+            'model',
+            'node',
+            'template',
+            'template.nodes',
+        ]);
+
+        $node = $onlyChildrenNodes
+            ? $this->getNoteFromStep($handler)
+            : $this->getNodeFromTemplate($handler);
+
+        throw_unless($node, new NodeDoesNotExistException(
+            "Node with handler \"{$handler}\" not found when conditionally initializing new steps.",
+        ));
+
+        $this->initializeStepForANode($node);
+    }
+
+    /**
+     * Get node from template.
+     *
+     * @param  class-string  $handler
+     */
+    private function getNodeFromTemplate(string $handler): ?Node
+    {
+        return $this->step->template->nodes->firstWhere('handler', $handler);
+    }
+
+    /**
+     * Get node from step.
+     *
+     * @param  class-string  $handler
+     */
+    private function getNoteFromStep(string $handler): ?Node
+    {
+        return $this->step->node;
+    }
+
+    /**
+     * Initialize step for a node.
+     */
+    private function initializeStepForANode(Node $node): void
+    {
+        // Do not init if step already exists
+        if ($this->stepAlreadyExistsForNode->handle($this->step, $node)) {
+
+            return;
+        }
+
+        $this->initializeStep->handle($this->step->model, $node);
+    }
+}
