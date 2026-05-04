5a6371371a0d5cf4939537ed49bf1cef5ce9ea88
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2026-05-04 13:25:56 +0200

    Update Laravel compatibility

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 
 use Dystcz\Flow\Tests\TestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
 uses(TestCase::class)->in('.');
-uses(DatabaseMigrations::class, LazilyRefreshDatabase::class)->in('Feature');
+uses(LazilyRefreshDatabase::class)->in('Feature');
